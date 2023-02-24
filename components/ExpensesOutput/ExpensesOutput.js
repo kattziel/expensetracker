e3b7a81c6db0d6c52211e5cf719f5c4661a82d80
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-02-24 18:43:02 +0100

    outputting data and basic styling

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,12 +1,50 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  const DUMMY_EXPENSES = [
+    {
+      id: "e1",
+      description: "pair of shoes",
+      amount: 59.99,
+      date: new Date("2023-02-24"),
+    },
+    {
+      id: "e2",
+      description: "food for dogs",
+      amount: 99.99,
+      date: new Date("2023-01-15"),
+    },
+    {
+      id: "e3",
+      description: "books",
+      amount: 29.99,
+      date: new Date("2023-02-14"),
+    },
+    {
+      id: "e4",
+      description: "phone",
+      amount: 599.99,
+      date: new Date("2023-02-18"),
+    },
+  ];
+
   return (
-    <View>
-      <ExpensesSummary/>
-      <ExpensesList/>
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES}/>
     </View>
   );
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+    flex: 1
+  }
+})
