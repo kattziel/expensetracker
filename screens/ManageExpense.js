98a393b29978a468b28563937b29e8541c26c090
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-06 12:40:00 +0100

    styling management screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,9 @@
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
 function ManageExpense() {
   return <Text>ManageExpense Screen</Text>;
 }
 
 export default ManageExpense;
+
+const style = StyleSheet.create({})
