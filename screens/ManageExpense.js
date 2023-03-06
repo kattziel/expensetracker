74185dedc6525208484bb44a5da883dc78061df1
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-06 13:03:21 +0100

    providing different modes

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,21 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  // we are using the expenseId param that is available here through route.params from ExpenseItem
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit expense" : "Add expense",
+    });
+    // navigation should be wrapped in useEffect or useLayout
+  }, [navigation, isEditing]);
+
   return <Text>ManageExpense Screen</Text>;
 }
 
 export default ManageExpense;
 
-const style = StyleSheet.create({})
+const style = StyleSheet.create({});
