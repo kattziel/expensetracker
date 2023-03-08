661b0d5e040ddf4d4eed76f18f919576dab92f3a
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-08 17:02:18 +0100

    configuring custom input

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -7,6 +7,7 @@ import { ExpensesContext } from "../store/expenses-context";
 
 import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
+import ExpenseForm from "../ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
 
@@ -54,6 +55,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm/>
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>Cancel</Button>
         <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
